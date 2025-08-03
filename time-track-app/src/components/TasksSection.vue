cb03cd38dca4ce9f8cba3eba6e39294ccb457327
Дмитрий Андреев <da616142@gmail.com>
2025-08-03 21:21:38 +0300

    Visual: add emtyListMessage

--- time-track-app/src/components/TasksSection.vue
+++ time-track-app/src/components/TasksSection.vue
@@ -1,34 +1,44 @@
 <script setup>
 import { defineProps, defineEmits } from 'vue'
 import { SUCCESS_BUTTON } from '../constans'
-
 import TaskCard from './TaskCard.vue'
 import BaseButton from './UI/BaseButton.vue'
+import { PlusCircleIcon } from '@heroicons/vue/24/outline'
 
 const props = defineProps({
-	tasks: Array,
+	tasks: { type: Array, default: () => [] },
 })
-
 const emit = defineEmits(['create', 'edit', 'delete', 'start', 'stop'])
 </script>
 
 <template>
-	<div class="col-span-2 bg-white border border-gray-200 rounded-2xl shadow-xl shadow-gray-300 p-4">
-		<div class="flex justify-end mb-4">
+	<div class="col-span-2 bg-white border border-gray-200 rounded-2xl shadow-xl p-4 flex flex-col">
+		<!-- Заголовок + кнопка -->
+		<div class="flex justify-end mb-4 flex-none">
 			<BaseButton :variant="SUCCESS_BUTTON" @click="emit('create')">
 				Создать задачу
 			</BaseButton>
 		</div>
-		<div class="flex flex-col gap-6 mt-6">
-			<TaskCard
-				v-for="task in tasks"
-				:key="task.id"
-				:task="task"
-				@edit="emit('edit', task)"
-				@delete="emit('delete', task)"
-				@start="$emit('start', $event)"
-  				@stop="$emit('stop', $event)"
-			/>
+		<div
+			class="flex-1 flex flex-col"
+			:class="tasks.length ? 'justify-start items-stretch' : 'justify-center items-center'"
+		>
+			<div v-if="tasks.length" class="w-full flex flex-col gap-6">
+				<TaskCard
+					v-for="task in tasks"
+					:key="task.id"
+					:task="task"
+					@edit="emit('edit', task)"
+					@delete="emit('delete', task)"
+					@start="$emit('start', $event)"
+					@stop="$emit('stop', $event)"
+				/>
+			</div>
+
+			<div v-else class="flex flex-col items-center space-y-4 text-gray-500">
+				<PlusCircleIcon class="w-16 h-16 text-gray-300" />
+				<p class="text-xl font-medium">Создайте новую задачу</p>
+			</div>
 		</div>
 	</div>
 </template>
--- time-track-app/src/components/TemplatesSection.vue
+++ time-track-app/src/components/TemplatesSection.vue
@@ -1,32 +1,45 @@
 <script setup>
 import { defineProps, defineEmits } from 'vue'
 import { SUCCESS_BUTTON } from '../constans'
-
 import TaskTemplateCard from './TaskTemplateCard.vue'
 import BaseButton from './UI/BaseButton.vue'
+import { ArrowTopRightOnSquareIcon } from '@heroicons/vue/24/outline'
 
 const props = defineProps({
-	templates: Array,
+	templates: { type: Array, default: () => [] },
 })
-
 const emit = defineEmits(['create', 'delete', 'use'])
 </script>
 
 <template>
-	<div class="col-span-1 bg-white border border-gray-200 rounded-2xl shadow-xl shadow-gray-300 p-4">
-		<div class="flex justify-end mb-4">
+	<div class="col-span-1 bg-white border border-gray-200 rounded-2xl shadow-xl p-4 flex flex-col">
+
+		<div class="flex justify-end mb-4 flex-none">
 			<BaseButton :variant="SUCCESS_BUTTON" @click="emit('create')">
 				Создать шаблон
 			</BaseButton>
 		</div>
-		<div class="flex flex-col gap-6 mt-6">
-			<TaskTemplateCard
-				v-for="template in templates"
-				:key="template.id"
-				:template="template"
-				@delete="emit('delete', template)"
-				@click="emit('use', template)"
-			/>
+
+		<div
+			class="flex-1 flex flex-col"
+			:class="
+				templates.length ? 'justify-start items-stretch' : 'justify-center items-center'
+			"
+		>
+			<div v-if="templates.length" class="w-full flex flex-col gap-6">
+				<TaskTemplateCard
+					v-for="template in templates"
+					:key="template.id"
+					:template="template"
+					@delete="emit('delete', template)"
+					@click="emit('use', template)"
+				/>
+			</div>
+
+			<div v-else class="flex flex-col items-center space-y-4 text-gray-500">
+				<ArrowTopRightOnSquareIcon class="w-16 h-16 text-gray-300" />
+				<p class="text-xl font-medium">Создайте новый шаблон</p>
+			</div>
 		</div>
 	</div>
 </template>
